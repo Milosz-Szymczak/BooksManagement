7e34dd0c2d2579e5a0c5679324c32002bc197ee6
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-10 14:34:37 +0200

    Added @Builder for tests.

--- src/main/java/pl/milosz/booksmanagement/model/Book.java
+++ src/main/java/pl/milosz/booksmanagement/model/Book.java
@@ -2,6 +2,7 @@ package pl.milosz.booksmanagement.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Table(name = "books")
 public class Book {
     @Id
