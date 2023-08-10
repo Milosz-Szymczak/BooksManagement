0d74ac7bed8e68d9deebcd05cae39886e934c9f6
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-10 12:57:52 +0200

    Added book model

---
+++ src/main/java/pl/milosz/booksmanagement/model/Book.java
@@ -0,0 +1,35 @@
+package pl.milosz.booksmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "book_name", nullable = false )
+    private String name;
+
+    @Column(name = "publisher", nullable = false )
+    private String publisher;
+
+    @Column(name = "author", nullable = false )
+    private String author;
+
+    @Column(name = "kind_of_book", nullable = false )
+    private String kind;
+
+    @Column(name = "release_date", nullable = false, length = 10)
+    private String releaseDate;
+
+    @Column(name = "isbn", nullable = false )
+    private long isbn;
+}
