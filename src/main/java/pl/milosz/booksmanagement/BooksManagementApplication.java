76d95d0bda3e38da0ea6e5d76d83d51822305e81
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-10 12:38:58 +0200

    Create project

---
+++ src/main/java/pl/milosz/booksmanagement/BooksManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.milosz.booksmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksManagementApplication.class, args);
+    }
+
+}
