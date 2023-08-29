7b65c17fa77523285685aaa4560968fe32504920
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-08-29 13:41:43 +0200

    Change url mapping and few method

--- src/main/java/pl/milosz/booksmanagement/cotroller/ManagementController.java
+++ src/main/java/pl/milosz/booksmanagement/cotroller/ManagementController.java
@@ -1,11 +1,18 @@
 package pl.milosz.booksmanagement.cotroller;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.milosz.booksmanagement.model.Book;
 import pl.milosz.booksmanagement.service.BookService;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.List;
 
 @Controller
@@ -17,11 +24,51 @@ public class ManagementController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/management-book")
-    public String getAllBook(Model model) {
-        List<Book> allBook = bookService.getAllBook();
-        model.addAttribute("books", allBook);
-        return "management-book";
+    @GetMapping("/management-check-book-admin")
+    public String getBooksForCheck(Model model) {
+        List<Book> allBook = bookService.getBooksWithoutConfirm();
+        if (allBook.isEmpty()) {
+            model.addAttribute("book", new Book());
+        } else {
+            model.addAttribute("books", allBook);
+        }
+        return "management-check-book-admin";
+    }
+
+    @GetMapping("/confirm-book/{id}")
+    public String confirmBook(@PathVariable Long id, @ModelAttribute("book") Book book, Model model) {
+        Book bookById = bookService.getBookById(id);
+        bookById.setConfirm(true);
+        bookService.updateBook(bookById);
+        return "redirect:/management-check-book-admin";
+    }
+
+    @GetMapping("/management-book-admin")
+    public String getAllConfirmBooksForAdmin(Model model) {
+        List<Book> allBook = bookService.getBooksWithConfirm();
+        if (allBook.isEmpty()) {
+            model.addAttribute("book", new Book());
+        } else {
+            model.addAttribute("books", allBook);
+        }
+        return "management-book-admin";
+    }
+
+    @GetMapping("/added-books")
+    public String getAllConfirmBooks(Model model) {
+        List<Book> allBook = bookService.getBooksWithConfirm();
+        if (allBook.isEmpty()) {
+           model.addAttribute("book", new Book());
+        } else {
+            model.addAttribute("books", allBook);
+        }
+        return "added-books";
+    }
+
+    @GetMapping("/form-update-book/{id}")
+    public String editBook(@PathVariable Long id, Model model) {
+        model.addAttribute("book", bookService.getBookById(id));
+        return "form-update-book";
     }
 
     @PostMapping("/form-update-book/{id}")
@@ -33,22 +80,15 @@ public class ManagementController {
         existBook.setIsbn(book.getIsbn());
         existBook.setPublisher(book.getPublisher());
         existBook.setReleaseDate(book.getReleaseDate());
-        existBook.setAmount(book.getAmount());
 
         bookService.updateBook(existBook);
-        return "redirect:/management-book";
-    }
-
-    @GetMapping("/form-update-book/{id}")
-    public String editBook(@PathVariable Long id, Model model) {
-        model.addAttribute("book", bookService.getBookById(id));
-        return "form-update-book";
+        return "redirect:/management-book-admin";
     }
 
     @GetMapping("/delete/{id}")
     public String deleteBook(@PathVariable Long id, Model model) {
         bookService.deleteBook(id);
-        return "redirect:/management-book";
+        return "redirect:/management-book-admin";
     }
 
     @GetMapping("/form-add-book")
@@ -60,7 +100,7 @@ public class ManagementController {
     @PostMapping("/form-add-book")
     public String saveBook(@ModelAttribute("book") Book book) {
         bookService.saveBook(book);
-        return "redirect:/management-book";
+        return "redirect:/added-books";
     }
 
     @GetMapping("/form-google-api")
@@ -68,4 +108,29 @@ public class ManagementController {
         return "form-google-api";
     }
 
+    @PostMapping("/form-search-book")
+    public String redirectToUrl(@RequestParam("name") URL name) throws IOException {
+        try (InputStream input = name.openStream()) {
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder json = new StringBuilder();
+            String t;
+            int c;
+            while ((c = reader.read()) != -1) {
+                json.append((char) c);
+            }
+            reader.close();
+            isr.close();
+
+            t = json.toString();
+
+            JsonObject jsonObject = JsonParser.parseString(t).getAsJsonObject();
+
+            String title = jsonObject.getAsJsonObject("volumeInfo").get("title").getAsString();
+
+            System.out.println(title);
+
+        }
+        return "form-google-api";
+    }
 }
