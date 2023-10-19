e92fa2202266eb2517d254930d68295475f4ac2b
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-10-19 13:15:24 +0200

    Refactoring class

--- src/main/java/pl/milosz/booksmanagement/service/impl/GoogleBookServiceImpl.java
+++ src/main/java/pl/milosz/booksmanagement/service/impl/GoogleBookServiceImpl.java
@@ -7,6 +7,7 @@ import pl.milosz.booksmanagement.dto.googleBook.GoogleBookDto;
 import pl.milosz.booksmanagement.service.GoogleBookService;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -19,74 +20,93 @@ public class GoogleBookServiceImpl implements GoogleBookService {
 
     @Override
     public Map<String, GoogleBookDto> getAllGoogleBooks(String title) {
+
         Map<String, GoogleBookDto> allGoogleBooks = new HashMap<>();
         try {
-            String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + title;
-            URL url = new URL(apiUrl);
+            HttpURLConnection connection = connectWithGoogleApi(title);
 
-            // Otwieranie połączenia HTTP
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            // Odczytywanie odpowiedzi
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                // Parsowanie JSON
-                JSONObject jsonResponse = new JSONObject(response.toString());
-                JSONArray items = jsonResponse.getJSONArray("items");
-
-                for (int i = 0; i < items.length(); i++) {
-                    JSONObject book = items.getJSONObject(i);
-                    String bookID = book.getString("id");
-                    JSONObject volumeInfo = book.getJSONObject("volumeInfo");
-                    String bookTitle = volumeInfo.getString("title");
-                    String bookSubTitle;
-                    if (volumeInfo.has("subtitle")) {
-                        bookSubTitle = volumeInfo.getString("subtitle");
-                    } else {
-                        bookSubTitle = "not exist";
-                    }
-                    ArrayList<String> bookAuthors = new ArrayList<>();
-                    if (volumeInfo.has("authors")) {
-                        JSONArray authors = volumeInfo.getJSONArray("authors");
-                        for (int j = 0; j < authors.length(); j++) {
-                            bookAuthors.add(authors.getString(j));
-                        }
-                    } else {
-                        bookAuthors.add("not exist");
-                    }
-                    JSONArray identifiersArray = volumeInfo.getJSONArray("industryIdentifiers");
-                    String languageBook = volumeInfo.getString("language");
-                    String isbn10Identifier = null;
-                    for (int j = 0; j < identifiersArray.length(); j++) {
-                        JSONObject identifierObject = identifiersArray.getJSONObject(j);
-                        if ("ISBN_10".equals(identifierObject.getString("type"))) {
-                            isbn10Identifier = identifierObject.getString("identifier");
-                            break;
-                        }
-                    }
-                    String bookPublisherDate = volumeInfo.getString("publishedDate");
-
-                    allGoogleBooks.put(bookID, new GoogleBookDto(bookID, bookTitle, bookSubTitle, bookAuthors, bookPublisherDate, isbn10Identifier, languageBook));
-                }
-            } else {
-                System.out.println("Błąd podczas pobierania danych: " + responseCode);
-            }
+            searchGoogleBook(connection, allGoogleBooks);
 
             connection.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return allGoogleBooks;
+    }
+
+    private static void searchGoogleBook(HttpURLConnection connection, Map<String, GoogleBookDto> allGoogleBooks) throws IOException {
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            JSONArray items = getListOfBooks(connection);
+
+            getAllBooks(items, allGoogleBooks);
+        } else {
+            System.out.println("Błąd podczas pobierania danych: " + responseCode);
+        }
+    }
+
+    private static HttpURLConnection connectWithGoogleApi(String title) throws IOException {
+        String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + title;
+        URL url = new URL(apiUrl);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        return connection;
+    }
+
+    private static JSONArray getListOfBooks(HttpURLConnection connection) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        JSONObject jsonResponse = new JSONObject(response.toString());
+        JSONArray items = jsonResponse.getJSONArray("items");
+        return items;
+    }
+
+    private static void getAllBooks(JSONArray items, Map<String, GoogleBookDto> allGoogleBooks) {
+
+        for (int i = 0; i < items.length(); i++) {
+            JSONObject book = items.getJSONObject(i);
+
+            String bookID = book.has("id") ? book.getString("id") : "";
+            JSONObject volumeInfo = book.has("volumeInfo") ? book.getJSONObject("volumeInfo") : new JSONObject();
+            String bookTitle = volumeInfo.has("title") ? volumeInfo.getString("title") : "";
+            String bookSubTitle = volumeInfo.has("subtitle") ? volumeInfo.getString("subtitle") : "";
+            String publisher = volumeInfo.has("publisher") ? volumeInfo.getString("publisher") : "";
+            ArrayList<String> bookAuthors = new ArrayList<>();
+            JSONArray authors = volumeInfo.has("authors") ? volumeInfo.getJSONArray("authors") : new JSONArray();
+            if (!authors.isEmpty()) {
+                for (int j = 0; j < authors.length(); j++) {
+                    bookAuthors.add(authors.getString(j));
+                }
+            }
+
+            JSONArray identifiersArray = volumeInfo.has("industryIdentifiers") ?
+                    volumeInfo.getJSONArray("industryIdentifiers") : new JSONArray();
+
+            String languageBook = volumeInfo.has("language") ? volumeInfo.getString("language") : "";
+
+            String isbn10Identifier = null;
+            for (int j = 0; j < identifiersArray.length(); j++) {
+                JSONObject identifierObject = identifiersArray.getJSONObject(j);
+
+                if ("ISBN_10".equals(identifierObject.getString("type"))) {
+                    isbn10Identifier = identifierObject.getString("identifier");
+                    break;
+                }
+            }
+            String bookPublisherDate = volumeInfo.has("publishedDate") ?
+                    volumeInfo.getString("publishedDate") : "";
+
+            allGoogleBooks.put(bookID, new GoogleBookDto(bookID, bookTitle, bookSubTitle, publisher, bookAuthors, bookPublisherDate, isbn10Identifier, languageBook));
+        }
     }
 }
