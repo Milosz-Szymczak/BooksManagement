56ee1db2a0e113aa0b8e5037037dbe362174598d
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-11-18 20:36:23 +0100

    Change 2 methods to void

--- src/main/java/pl/milosz/booksmanagement/service/BookService.java
+++ src/main/java/pl/milosz/booksmanagement/service/BookService.java
@@ -2,20 +2,19 @@ package pl.milosz.booksmanagement.service;
 
 import org.springframework.stereotype.Service;
 import pl.milosz.booksmanagement.dto.BookDto;
-import pl.milosz.booksmanagement.model.Book;
 
 import java.util.List;
 
 @Service
 public interface BookService {
-    BookDto saveBook(BookDto bookDto);
+    void saveBook(BookDto bookDto);
 
     List<BookDto> getBooksWithoutConfirm();
     List<BookDto> getBooksWithConfirm();
 
     BookDto getBookById(Long id);
 
-    BookDto updateBook(BookDto bookDto);
+    void updateBook(BookDto bookDto);
 
     void deleteBook(Long id);
 }
