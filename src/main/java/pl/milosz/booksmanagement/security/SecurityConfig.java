a2e02083deedea2a3c1cbed3e8cc3a3cf3bff2a7
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2023-11-30 17:10:51 +0100

    Comment on all lines

--- src/main/java/pl/milosz/booksmanagement/security/SecurityConfig.java
+++ src/main/java/pl/milosz/booksmanagement/security/SecurityConfig.java
@@ -1,23 +1,23 @@
-package pl.milosz.booksmanagement.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable);
-                //.formLogin(AbstractHttpConfigurer::disable);
-        return http.build();
-
-    }
-}
+//package pl.milosz.booksmanagement.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable);
+//                //.formLogin(AbstractHttpConfigurer::disable);
+//        return http.build();
+//
+//    }
+//}
