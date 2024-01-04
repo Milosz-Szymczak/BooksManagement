af24afacf628e56674084ed25656998a94caf5e6
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-01-04 22:04:28 +0100

    Add getAllUser() method

--- src/main/java/pl/milosz/booksmanagement/security/service/UserService.java
+++ src/main/java/pl/milosz/booksmanagement/security/service/UserService.java
@@ -1,10 +1,14 @@
-package pl.milosz.booksmanagement.service;
+package pl.milosz.booksmanagement.security.service;
 
 
 import pl.milosz.booksmanagement.model.user.User;
 
+import java.util.List;
+
 public interface UserService {
 
     void saveUser(User user);
+
+    List<User> getAllUsers();
 
 }
---
+++ src/main/java/pl/milosz/booksmanagement/security/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package pl.milosz.booksmanagement.security.service;
+
+import org.springframework.stereotype.Service;
+import pl.milosz.booksmanagement.model.user.User;
+import pl.milosz.booksmanagement.repository.UserRepository;
+import pl.milosz.booksmanagement.security.config.SecurityConfig;
+
+import java.util.List;
+
+@Service
+class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void saveUser(User user) {
+        user.setRole(User.Role.USER);
+        userRepository.save(user);
+
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
