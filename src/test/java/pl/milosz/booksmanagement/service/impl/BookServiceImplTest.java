d83f23f8c67a344d9748bccc19872ec214856702
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-01-07 18:17:31 +0100

    Add test

--- src/test/java/pl/milosz/booksmanagement/service/impl/BookServiceImplTest.java
+++ src/test/java/pl/milosz/booksmanagement/service/impl/BookServiceImplTest.java
@@ -1,19 +1,24 @@
 package pl.milosz.booksmanagement.service.impl;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.milosz.booksmanagement.dto.BookDto;
-import pl.milosz.booksmanagement.model.Book;
-import pl.milosz.booksmanagement.repository.BooksRepository;
+import pl.milosz.booksmanagement.model.book.Book;
+import pl.milosz.booksmanagement.model.book.Kind;
+import pl.milosz.booksmanagement.repository.BookRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -22,7 +27,7 @@ import static org.mockito.Mockito.*;
 class BookServiceImplTest {
 
     @Mock
-    private BooksRepository booksRepository;
+    private BookRepository bookRepository;
     @InjectMocks
     private BookServiceImpl bookServiceImpl;
 
@@ -31,83 +36,87 @@ class BookServiceImplTest {
 
     @BeforeEach
     public void init() {
-        bookDto = BookDto.builder().id(1L).title("Clean Code").publisher("Helion").author("Martin Robert C.")
-                .kind("Computer Science").releaseDate("2009-03-01").isbn("9780132350884").language("pl").confirm(true).build();
-        book = Book.builder().id(2L).title("Clean Code").publisher("Helion").author("Martin Robert C.")
-                .kind("Computer Science").releaseDate("2009-03-01").isbn("9780132350884").language("pl").confirm(true).build();
+        bookDto = BookDto.builder().title("Clean Code").publisher("Helion").author("Martin Robert C.")
+                .kind(Kind.FANTASY).releaseDate("2009-03-01").isbn("9780132350884").language("pl").confirm(false).build();
+        book = Book.builder().title("Clean Code").publisher("Helion").author("Martin Robert C.")
+                .kind(Kind.HISTORY).releaseDate("2009-03-01").isbn("9780132350884").language("pl").confirm(false).build();
     }
 
     @Test
-    void saveBook_shouldUpdateBookSuccessfully() {
-        when(booksRepository.save(any(Book.class))).thenReturn(book);
+    void saveBook_shouldSaveBookSuccessfully() {
+        // Arrange
+        when(bookRepository.save(any(Book.class))).thenReturn(book);
 
-        BookDto savedBook = bookServiceImpl.saveBook(bookDto);
+        // Act
+        bookServiceImpl.saveBook(bookDto);
 
-        assertThat(savedBook).isNotNull();
-        assertThat(savedBook).isEqualTo(bookDto);
+        // Assert
+        ArgumentCaptor<Book> bookCaptor = ArgumentCaptor.forClass(Book.class);
+        verify(bookRepository).save(bookCaptor.capture());
 
-        verify(booksRepository).save(any(Book.class));
+        Book bookCaptorValue = bookCaptor.getValue();
+        assertFalse(bookCaptorValue.isConfirm());
+        assertEquals(bookDto.getTitle(), bookCaptorValue.getTitle());
     }
 
     @Test
-    void getBooksWithoutConfirm_shouldReturnStreamOfBooksWithoutConfirmInDataBase() {
-        Book bookWithoutConfirm = book;
-        bookWithoutConfirm.setConfirm(false);
-        List<Book> listOfBooks = List.of(bookWithoutConfirm,bookWithoutConfirm);
+    void getBooksNotConfirm_shouldReturnStreamOfBooksWithoutConfirmInDataBase() {
+        List<Book> listOfBooks = List.of(book, book);
 
-        when(booksRepository.findAll()).thenReturn(listOfBooks);
+        when(bookRepository.getBooksNotConfirm()).thenReturn(listOfBooks);
 
-        List<BookDto> booksWithoutConfirm = bookServiceImpl.getBooksWithoutConfirm();
+        List<BookDto> booksNotConfirm = bookServiceImpl.getBooksNotConfirm();
 
-        assertThat(booksWithoutConfirm).isNotNull();
-        assertThat(booksWithoutConfirm.size()).isEqualTo(2);
-        assertThat(booksWithoutConfirm.get(0).isConfirm()).isEqualTo(false);
+        assertThat(booksNotConfirm).isNotNull();
+        assertThat(booksNotConfirm.size()).isEqualTo(2);
+        assertThat(booksNotConfirm.get(0).isConfirm()).isEqualTo(false);
 
-        verify(booksRepository).findAll();
+        verify(bookRepository).getBooksNotConfirm();
     }
 
     @Test
-    void getBooksWithConfirm_shouldReturnStreamOfBooksWithConfirmInDataBase() {
+    void getConfirmBooks_shouldReturnStreamOfBooksWithConfirmInDataBase() {
+        book.setConfirm(true);
         List<Book> listOfBooks = List.of(book);
-        when(booksRepository.findAll()).thenReturn(listOfBooks);
+        when(bookRepository.getConfirmBooks()).thenReturn(listOfBooks);
 
-        List<BookDto> booksWithConfirm = bookServiceImpl.getBooksWithConfirm();
+        List<BookDto> booksWithConfirm = bookServiceImpl.getConfirmBooks();
 
         assertThat(booksWithConfirm).isNotNull();
         assertThat(booksWithConfirm.size()).isEqualTo(1);
         assertThat(booksWithConfirm.get(0).isConfirm()).isEqualTo(true);
 
-        verify(booksRepository).findAll();
+        verify(bookRepository).getConfirmBooks();
     }
 
     @Test
     void getBookById_shouldReturnBookDtoById() {
-        when(booksRepository.findById(book.getId())).thenReturn(Optional.ofNullable(book));
+        when(bookRepository.findById(book.getId())).thenReturn(Optional.ofNullable(book));
 
         BookDto bookById = bookServiceImpl.getBookById(book.getId());
 
         assertThat(bookById).isNotNull();
         assertThat(bookById.getTitle()).isEqualTo("Clean Code");
 
-        verify(booksRepository).findById(book.getId());
+        verify(bookRepository).findById(book.getId());
     }
 
     @Test
     void updateBook_shouldUpdateBookSuccessfully() {
-        when(booksRepository.save(any(Book.class))).thenReturn(book);
+        when(bookRepository.save(any(Book.class))).thenReturn(book);
 
-        bookServiceImpl.updateBook(bookDto);
+        bookServiceImpl.updateBook(bookDto.getId(), bookDto);
 
-        verify(booksRepository).save(any(Book.class));
+        verify(bookRepository).save(any(Book.class));
     }
 
     @Test
     void deleteBook_shouldDeleteBookSuccessfully() {
-        when(booksRepository.findById(book.getId())).thenReturn(Optional.ofNullable(book));
+        when(bookRepository.findById(book.getId())).thenReturn(Optional.ofNullable(book));
 
         bookServiceImpl.deleteBook(book.getId());
 
-        verify(booksRepository).findById(book.getId());
-        verify(booksRepository).delete(book);
+        verify(bookRepository).findById(book.getId());
+        verify(bookRepository).delete(book);
     }
 }
