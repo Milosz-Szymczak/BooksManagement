0eaf1bcda165c8066ebf8dec1b06affcfaf2feb2
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-01-12 14:32:27 +0100

    Add global exception for NoSuchElementException

---
+++ src/main/java/pl/milosz/booksmanagement/exception/controller/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package pl.milosz.booksmanagement.exception.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Resource not found: " + ex.getMessage());
+    }
+}
