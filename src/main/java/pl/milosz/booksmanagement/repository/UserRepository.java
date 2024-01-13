1fca030f0a10312738fe5d57c4809566d58ad662
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-01-13 15:46:37 +0100

    Add findByUsername and findByRole

--- src/main/java/pl/milosz/booksmanagement/repository/UserRepository.java
+++ src/main/java/pl/milosz/booksmanagement/repository/UserRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.milosz.booksmanagement.model.user.User;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends ListCrudRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    Optional<User> findByRole(User.Role role);
 }
