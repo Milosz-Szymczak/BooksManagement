ca3c24b008a8016e2fb3ceb856cb7fd4f087134b
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-01-13 15:42:56 +0100

    Add userService bean for identity books, and upgrade hasRole to  hasAnyRole

--- src/main/java/pl/milosz/booksmanagement/controller/BookController.java
+++ src/main/java/pl/milosz/booksmanagement/controller/BookController.java
@@ -8,19 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.milosz.booksmanagement.dto.BookDto;
-import pl.milosz.booksmanagement.model.book.Book;
 import pl.milosz.booksmanagement.model.book.Kind;
+import pl.milosz.booksmanagement.model.user.User;
+import pl.milosz.booksmanagement.security.service.UserService;
 import pl.milosz.booksmanagement.service.BookService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BookController {
 
     private final BookService bookService;
+    private final UserService userService;
 
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, UserService userService) {
         this.bookService = bookService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
@@ -34,7 +38,7 @@ public class BookController {
         return "visitor/listBooks";
     }
 
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/addBookForm")
     public String createBookForm(Model model) {
         model.addAttribute("kind", Kind.values());
@@ -42,10 +46,29 @@ public class BookController {
         return "user/addBookForm";
     }
 
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @PostMapping("/addBookForm")
     public String saveBook(@ModelAttribute("book") BookDto bookDto) {
+
+        Optional<User> loggedInUser = userService.findLoggedUser();
+        loggedInUser.ifPresent(bookDto::setUser);
+
         bookService.saveBook(bookDto);
         return "redirect:/";
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping("/profile")
+    public String getBooksAddedByUser(Model model) {
+        Optional<User> loggedInUser = userService.findLoggedUser();
+
+
+        List<BookDto> allBookDto = bookService.getBooksAddedByUser(loggedInUser.get().getUsername());
+        if (allBookDto.isEmpty()) {
+            model.addAttribute("book", new BookDto());
+        } else {
+            model.addAttribute("books", allBookDto);
+        }
+        return "user/profile";
     }
 }
